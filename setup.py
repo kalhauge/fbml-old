2ee0e4c54d84c311b4ee8f4e152f43ec7a970dc7
Christian Gram Kalhauge <christian@kalhauge.dk>
2013-06-05 11:23:04 +0200

    Small restructuring

--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ setup(
     description='Python implementaion of the Flow Based Modeling Language (FBML)',
     author='Christian Gram Kalhauge',
     author_email='christian@kalhauge.dk',
+    long_description=open(README.rst).read(),
+    scripts=[
+        'bin/compilers/flow2dot',
+        'bin/optimizers/fbml-type'
+        ],
     packages=['pyfbml'],
 )
